809bf85f14a06ac3daac5d64ffd7b654ff8a969d
Nicole Prati <nicole.e.prati@gmail.com>
2024-01-16 14:39:25 -0500

    Hero Video section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import './Navbar.css'
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import './Navbar.css'
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -31,16 +31,16 @@ function Navbar() {
             TRVL
             <i class="fab fa-typo3" />
           </Link>
-          <div className="manu-icon" onClick={handleClick}>
+          <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-itme">
+            <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
-            <li className="nav-itme">
+            <li className="nav-item">
               <Link
                 to="/services"
                 className="nav-links"
@@ -49,7 +49,7 @@ function Navbar() {
                 Services
               </Link>
             </li>
-            <li className="nav-itme">
+            <li className="nav-item">
               <Link
                 to="/products"
                 className="nav-links"
@@ -58,7 +58,7 @@ function Navbar() {
                 Products
               </Link>
             </li>
-            <li className="nav-itme">
+            <li className="nav-item">
               <Link
                 to="/sign-up"
                 className="nav-links-mobile"
---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+
+
+
+const Home = () => {
+  return (
+    <>
+    <HeroSection />
+
+    </>
+
+
+  )
+}
+
+export default Home
